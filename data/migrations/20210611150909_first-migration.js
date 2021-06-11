58f0d000dec39c8ed84a947f810ba872cbb1a491
devin mitchell <devin.a.mitchell@gmail.com>
2021-06-11 09:25:57 -0600

    forgot the .down function in the migration oops!

--- data/migrations/20210611150909_first-migration.js
+++ data/migrations/20210611150909_first-migration.js
@@ -45,5 +45,9 @@ exports.up = function(knex) {
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTableIfExists('project_resources')
+    .dropTableIfExists('tasks')
+    .dropTableIfExists('resources')
+    .dropTableIfExists('projects')
 };
