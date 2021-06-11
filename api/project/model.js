034ebbb705887c2abe2983dd02bf2584dc4be52c
devin mitchell <devin.a.mitchell@gmail.com>
2021-06-11 13:12:50 -0600

    finished project endpoints and all tests are passing

--- api/project/model.js
+++ api/project/model.js
@@ -1,11 +1,41 @@
 const db = require('../../data/dbConfig')
 
-function getProjects() {
-    return db('projects')
+async function getProjects() {
+    let projects = await db('projects')
+        .select(
+            'project_name',
+            'project_description',
+            'project_completed'
+        )
+    
+    projects.map(proj => {
+        if (proj.project_completed === 0) {
+            return proj.project_completed = false
+        } else {
+            return proj.project_completed = true
+        }
+    })
+    return projects
 }
 
-function createProject(newProject) {
+async function getProjectById(project_id) {
+    const proj = await db('projects')
+        .where({ project_id })
+        .first()
 
+        if (proj.project_completed === 0) {
+            proj.project_completed = false
+        } else {
+            proj.project_completed = true
+        }
+    return proj
+    }
+
+async function createProject(newProject) {
+    const [id] = await db('projects')
+        .insert(newProject)
+    
+    return getProjectById(id)
 }
 
 module.exports = {
--- api/project/router.js
+++ api/project/router.js
@@ -1,5 +1,22 @@
 const express = require('express')
+const ProjModel = require('./model')
 
-const projRouter = express.Router()
+const router = express.Router()
 
-module.exports = projRouter
+router.get('/', (req, res, next) => {
+    ProjModel.getProjects()
+        .then(projects => {
+            res.status(200).json(projects)
+        })
+        .catch(next)
+})
+
+router.post('/', (req, res, next) => {
+    ProjModel.createProject(req.body)
+        .then(project => {
+            res.status(201).json(project)
+        })
+        .catch(next)
+})
+
+module.exports = router
